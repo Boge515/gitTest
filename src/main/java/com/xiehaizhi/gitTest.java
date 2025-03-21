8760c9cac42d89e27870c13709282c3dbda83905
Boge515 <1530235225@qq.com>
2025-03-21 16:33:58 +0800

    push commit

--- src/main/java/com/xiehaizhi/gitTest.java
+++ src/main/java/com/xiehaizhi/gitTest.java
@@ -8,6 +8,7 @@ public class gitTest {
         System.out.println("hello git4 hot-fix");
         System.out.println(" master test");
         System.out.println("hello git5 hot-fix test");
+        System.out.println("push test");
 
     }
 }
